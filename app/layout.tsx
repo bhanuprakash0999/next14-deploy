f9a244e72a33e811723e2e3dde164d06f82ca509
bhanuprakash-sittaroj <bhanuprakash.sittaroj.voltus@amura.ai>
2024-01-25 17:41:08 +0530

    added: metatags v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 // import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -7,14 +7,17 @@ import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: {
-//     absolute: "Amura Health - Web",
-//     default: "default main",
-//     template: "route"
-//   },
-//   description: "Amura can help you get healthy and stay healthy in a way that has not been possible before this. Get Healthy Now",
-// };
+export const metadata: Metadata = {
+  title: {
+    absolute: "Amura Health - Web",
+    default: "default main",
+    template: "route"
+  },
+  description: "Amura can help you get healthy and stay healthy in a way that has not been possible before this. Get Healthy Now",
+  openGraph : {
+    images: "https://amura.ai/wp-content/uploads/2022/11/amura-logo.svg"
+  }
+};
 
 export default function RootLayout({
   children,
@@ -23,11 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <>
-     <Head>
-        <meta property="og:title" content="Amura Health - Web Assembler" />
-        <meta property="og:description" content="Amura can help you get healthy and stay healthy in a way that has not been possible before this. Get Healthy Now" />
-        <meta property="og:image" content="https://amura.ai/wp-content/uploads/2022/11/amura-logo.svg" />
-     </Head>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
